06cd2f123afe24ca029ec7ce1241eb9c51d8d44a
Ashmi Banerjee <ashmibanerjee1411@gmail.com>
2024-08-06 17:18:08 +0200

    first draft of the GPT 4 inference

--- src/__init__.py
+++ src/__init__.py
@@ -1,11 +1,11 @@
-from vectordb.vectordb import *
-from vectordb.helpers import *
-from vectordb.lancedb_init import * 
+from src.vectordb.vectordb import *
+from src.vectordb.helpers import *
+from src.vectordb.lancedb_init import *
 
-from sustainability.s_fairness import * 
-from information_retrieval.info_retrieval import *
-from augmentation.prompt_generation import * 
-from text_generation.model_init import * 
-from text_generation.text_generation import *
+from src.sustainability.s_fairness import *
+from src.information_retrieval.info_retrieval import *
+from src.augmentation.prompt_generation import *
+from src.text_generation.model_init import *
+from src.text_generation.text_generation import *
 
-from data_directories import *
+from src.data_directories import *
--- src/information_retrieval/info_retrieval.py
+++ src/information_retrieval/info_retrieval.py
@@ -1,14 +1,16 @@
-import sys 
-import os 
+import sys
+import os
 import re
-from vectordb import vectordb
-from sustainability import s_fairness
+sys.path.append("../")
+from src.vectordb import vectordb
+from src.sustainability import s_fairness
 import json
 
-import logging 
+import logging
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(encoding='utf-8', level=logging.DEBUG)
+
 
 def get_travel_months(query):
     """
@@ -20,33 +22,34 @@ def get_travel_months(query):
 
     """
     months = [
-        "January", "February", "March", "April", "May", "June", 
+        "January", "February", "March", "April", "May", "June",
         "July", "August", "September", "October", "November", "December"
     ]
 
     seasons = {
-            "spring": ["March", "April", "May"],
-            "summer": ["June", "July", "August"],
-            "fall": ["September", "October", "November"],
-            "autumn": ["September", "October", "November"],  
-            "winter": ["December", "January", "February"]
-        }
+        "spring": ["March", "April", "May"],
+        "summer": ["June", "July", "August"],
+        "fall": ["September", "October", "November"],
+        "autumn": ["September", "October", "November"],
+        "winter": ["December", "January", "February"]
+    }
 
     months_in_query = []
-    
+
     for month in months:
         if re.search(r'\b' + month + r'\b', query, re.IGNORECASE):
             months_in_query.append(month)
-    
+
     # Check for seasons in the query
     for season, season_months in seasons.items():
         if re.search(r'\b' + season + r'\b', query, re.IGNORECASE):
             months_in_query += season_months
-    
+
     # Return None if neither months nor seasons are found
     return months_in_query
 
-def get_wikivoyage_context(query, limit = 10, reranking = 0):
+
+def get_wikivoyage_context(query, limit=10, reranking=0):
     """
     
     Function to retrieve the relevant documents and listings from the wikivoyage database. Works in two steps: 
@@ -67,17 +70,17 @@ def get_wikivoyage_context(query, limit = 10, reranking = 0):
     logger.info("Finished getting chunked wikivoyage docs.")
 
     results = {}
-    for doc in docs: 
+    for doc in docs:
         results[doc['city']] = {key: value for key, value in doc.items() if key != 'city'}
         results[doc['city']]['listings'] = []
 
-    cities = [result['city'] for result in docs] 
+    cities = [result['city'] for result in docs]
 
     listings = vectordb.search_wikivoyage_listings(query, cities, limit, reranking)
     logger.info("Finished getting wikivoyage listings.")
     # logger.info(type(docs), type(listings))
 
-    for listing in listings: 
+    for listing in listings:
         # logger.info(listing['city'])
         results[listing['city']]['listings'].append({
             'type': listing['type'],
@@ -88,11 +91,13 @@ def get_wikivoyage_context(query, limit = 10, reranking = 0):
     logger.info("Returning retrieval results.")
     return results
 
+
 def get_sustainability_scores(query, destinations):
     """
     
-    Function to get the s-fairness scores for each destination for the given month (or the ideal month of travel if the user hasn't provided a month). 
-    If multiple months are provided (or season), then the month with the minimum s-fairness score is chosen for the city.
+    Function to get the s-fairness scores for each destination for the given month (or the ideal month of travel if
+    the user hasn't provided a month). If multiple months are provided (or season), then the month with the minimum
+    s-fairness score is chosen for the city.
 
     Args: 
         - query: str 
@@ -100,7 +105,7 @@ def get_sustainability_scores(query, destinations):
     
     """
 
-    result = [] #list of dicts of the format {city: <city>, month: <month>, }
+    result = []  # list of dicts of the format {city: <city>, month: <month>, }
     city_scores = {}
 
     months = get_travel_months(query)
@@ -110,7 +115,7 @@ def get_sustainability_scores(query, destinations):
         if city not in city_scores:
             city_scores[city] = []
 
-        if not months: # no month(s) or seasons provided by the user
+        if not months:  # no month(s) or seasons provided by the user
             city_scores[city].append(s_fairness.compute_sfairness_score(city))
         else:
             for month in months:
@@ -119,7 +124,7 @@ def get_sustainability_scores(query, destinations):
     logger.info("Finished getting s-fairness scores.")
 
     for city, scores in city_scores.items():
-        
+
         no_result = 0
         for score in scores:
             if not score['month']:
@@ -130,7 +135,7 @@ def get_sustainability_scores(query, destinations):
                     's-fairness': 'No data available'
                 })
                 break
-        
+
         if not no_result:
             min_score = min(scores, key=lambda x: x['s-fairness'])
             result.append({
@@ -141,6 +146,7 @@ def get_sustainability_scores(query, destinations):
 
     logger.info("Returning s-fairness results.")
     return result
+
 
 def get_cities(context):
     """
@@ -153,13 +159,13 @@ def get_cities(context):
 
     recommended_cities = []
 
-    for city, info in context.items(): 
+    for city, info in context.items():
         city_info = {
             'city': city,
             'country': info['country']
         }
 
-        if "sustainability" in info: 
+        if "sustainability" in info:
             city_info['month'] = info['sustainability']['month']
             city_info['s-fairness'] = info['sustainability']['s-fairness']
 
@@ -167,12 +173,14 @@ def get_cities(context):
 
     sorted_cities = sorted(recommended_cities, key=lambda x: x['s-fairness'])
     return sorted_cities
-        
+
 
 def get_context(query, **params):
     """
     
-    Function that returns all the context: from the database, as well as the respective s-fairness scores for the destinations. The default does not consider S-Fairness scores, i.e. to append sustainability scores, a non-zero parameter "sustainability" needs to be explicitly passed to params.
+    Function that returns all the context: from the database, as well as the respective s-fairness scores for the
+    destinations. The default does not consider S-Fairness scores, i.e. to append sustainability scores, a non-zero
+    parameter "sustainability" needs to be explicitly passed to params.
 
     Args:
         - query: str
@@ -180,13 +188,13 @@ def get_context(query, **params):
     
     """
 
-    limit = 3 
+    limit = 3
     reranking = 1
 
     if 'limit' in params:
-        limit = params['limit'] 
-    
-    if 'reranking' in params: 
+        limit = params['limit']
+
+    if 'reranking' in params:
         reranking = params['reranking']
 
     wikivoyage_context = get_wikivoyage_context(query, limit, reranking)
@@ -195,7 +203,7 @@ def get_context(query, **params):
     if 'sustainability' in params and params['sustainability']:
         s_fairness_scores = get_sustainability_scores(query, recommended_cities)
 
-        for score in s_fairness_scores: 
+        for score in s_fairness_scores:
             wikivoyage_context[score['city']]['sustainability'] = {
                 'month': score['month'],
                 's-fairness': score['s-fairness']
@@ -203,22 +211,24 @@ def get_context(query, **params):
 
     return wikivoyage_context
 
+
 def test():
     queries = []
-    query = "Suggest some places to visit during winter. I like hiking, nature and the mountains and I enjoy skiing in winter."
+    query = "Suggest some places to visit during winter. I like hiking, nature and the mountains and I enjoy skiing " \
+            "in winter. "
 
     context = None
 
-    try: 
+    try:
         context = get_context(query)
     except FileNotFoundError as e:
-        try: 
+        try:
             vectordb.create_wikivoyage_docs_db_and_add_data()
-            vectordb.create_wikivoyage_listings_db_and_add_data() 
+            vectordb.create_wikivoyage_listings_db_and_add_data()
 
             try:
                 context = get_context(query)
-            except Exception as e: 
+            except Exception as e:
                 exc_type, exc_obj, exc_tb = sys.exc_info()
                 fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
                 logger.error(f"Error while getting context: {e}, {(exc_type, fname, exc_tb.tb_lineno)}")
@@ -226,16 +236,17 @@ def test():
         except Exception as e:
             logger.error(f"Error while creating DB: {e}")
 
-    except Exception as e: 
+    except Exception as e:
         exc_type, exc_obj, exc_tb = sys.exc_info()
         fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
         logger.error(f"Error while getting context: {e}, {(exc_type, fname, exc_tb.tb_lineno)}")
-    
+
     file_path = os.path.join(os.getcwd(), "test_results", "test_result.json")
-    with open(file_path, 'w') as file: 
+    with open(file_path, 'w') as file:
         json.dump(context, file)
 
     return context
+
 
 if __name__ == "__main__":
     context = test()
