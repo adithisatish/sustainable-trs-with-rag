d056edadcfde3c317257a8244f0b77e63eb7f1e1
adithisatish <adithi.satish15@gmail.com>
2024-08-06 15:35:30 +0200

    added results for 20 more prompts

--- tests/test_script.py
+++ tests/test_script.py
@@ -28,18 +28,23 @@ logging.basicConfig(encoding='utf-8', level=logging.DEBUG)
 MODEL_NAMES = ['Llama3', 'Mistral', 'Gemma2', 'Llama3.1']
 INSTRUCTION_TUNED_MODELS = ['Llama3-Instruct', 'Mistral-Instruct', 'Llama3.1-Instruct', ]
 
-def test():
+def test(prompt_file_name, results_dir, start_idx, end_idx, sustainability=0):
     """
     
     Test function - runs the pipeline twice (once without sustainability, once with sustainability enabled) for 10 prompts for each model and stores the results. 
     The default settings of limit/k = 5 and reranking = 0 for the retrieval are used. The argument "test" in the pipeline must be set to True so that the retrieved list of cities are returned along with the LLM response.
 
+    Args: 
+        - prompt_file_name: str
+        - start_idx: int
+        - end_idx: int
+
     """
-    with open("prompts_100.json","r") as file:
+    with open(f"prompts/{prompt_file_name}","r") as file:
         prompts = json.load(file)
     
     # print(prompts)
-    prompts = prompts[:50] # Only because of limited GPU space; run the next 50 in the next iteration
+    prompts = prompts[start_idx:end_idx] # Only because of limited GPU space; 
     
     for model_name in INSTRUCTION_TUNED_MODELS: # To run the normal models, switch "INSTRUCTION_TUNED" to "MODEL_NAMES"
         if 'Llama3.1' in model_name:
@@ -47,18 +52,32 @@ def test():
         else:
             dir_name = model_name.lower()
 
-        results_dir = os.path.join(os.getcwd(), "results-06.08.", dir_name)
+        results_dir = os.path.join(os.getcwd(), {results_dir}, dir_name) # change for new iteration
 
         for i, item in enumerate(prompts):
-            logger.info(f"Prompt {i}")
+            
+            logger.info(f"Prompt {i + start_idx}") #change this after script executes
+            prompt_results_dir = os.path.join(results_dir, f"prompt_{i+1+ start_idx}") 
+
             try:
-                logger.info(f"Running pipeline for {model_name} without sustainability..")
-                cities, response = pipeline(
-                    query = item['prompt'],
-                    model_name = model_name,
-                    test = 1,
-                    limit=10,
-                )
+                if not sustainability:
+                    logger.info(f"Running pipeline for {model_name} without sustainability..")
+                    cities, response = pipeline(
+                        query = item['prompt'],
+                        model_name = model_name,
+                        test = 1,
+                        limit=10,
+                    )
+                else: 
+                    logger.info(f"Running pipeline for {model_name} with sustainability..")
+                    cities, response = pipeline(
+                        query = item['prompt'],
+                        model_name = model_name,
+                        test = 1,
+                        sustainability = 1,
+                        limit=10, # RUN THIS AGAIN => param was not included in results-06.08. !!!
+                    )
+
             except Exception as e:
                 exc_type, exc_obj, exc_tb = sys.exc_info()
                 fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
@@ -66,48 +85,26 @@ def test():
             
             else:
                 logger.info("Pipeline execution complete. Storing response..")
-
-                prompt_results_dir = os.path.join(results_dir, f"prompt_{i+1}")
-
-                if not os.path.exists(prompt_results_dir):
-                    # Create the folder and any necessary intermediate directories
-                    os.makedirs(prompt_results_dir)
-
-                with open(f"{prompt_results_dir}/response.txt", "w") as f:
-                    f.write(response)
-
-                with open(f"{prompt_results_dir}/cities.json", 'w') as file: 
-                    json.dump(cities, file)
-            
-            # with sustainability
-            try:
-                logger.info(f"Running pipeline for {model_name} with sustainability..")
-                cities, response = pipeline(
-                    query = item['prompt'],
-                    model_name = model_name,
-                    test = 1,
-                    sustainability = 1
-                )
-            except Exception as e:
-                exc_type, exc_obj, exc_tb = sys.exc_info()
-                fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-                logger.error(f"Error while running pipeline: {e}, {(exc_type, fname, exc_tb.tb_lineno)}")
-            
-            else:
-                logger.info("Pipeline execution complete. Storing response..")
+                filenames = ['response.txt','cities.json']
+                
+                if sustainability:
+                    filenames = ['response_sustainable.txt','cities_sustainable.json']
 
                 if not os.path.exists(prompt_results_dir):
                     # Create the folder and any necessary intermediate directories
                     os.makedirs(prompt_results_dir)
 
-                with open(f"{prompt_results_dir}/response_sustainable.txt", "w") as f:
+                with open(f"{prompt_results_dir}/{filenames[0]}", "w") as f:
                     f.write(response)
 
-                with open(f"{prompt_results_dir}/cities_sustainable.json", 'w') as file: 
+                with open(f"{prompt_results_dir}/{filenames[1]}", 'w') as file: 
                     json.dump(cities, file)
 
                 logger.info(f"Stored response in {prompt_results_dir}")
+                
 
 
 if __name__ == "__main__":
+    # NEEDS TO BE RUN 
+
     test()
